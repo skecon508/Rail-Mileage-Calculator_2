8e128fe00505324b2909525d32adc86008ffabb6
SK <42920058+skecon508@users.noreply.github.com>
2025-11-07 17:46:00 -0500

    Update railappfinal.py

--- railappfinal.py
+++ railappfinal.py
@@ -169,7 +169,7 @@ with st.expander("Lookup Nodes by Filters"):
         filtered_nodes = filtered_nodes[filtered_nodes["STATE"] == state_sel]
 
     if div_sel and div_sel != "All":
-        edge_nodes = edges.loc[edges["DIVISION"] == subdiv_sel, ["FRFRANODE", "TOFRANODE"]]
+        edge_nodes = edges.loc[edges["DIVISION"] == div_sel, ["FRFRANODE", "TOFRANODE"]]
         valid_node_ids = set(edge_nodes["FRFRANODE"].astype(str)) | set(edge_nodes["TOFRANODE"].astype(str))
         filtered_nodes = filtered_nodes[filtered_nodes["FRANODEID"].astype(str).isin(valid_node_ids)]
 
